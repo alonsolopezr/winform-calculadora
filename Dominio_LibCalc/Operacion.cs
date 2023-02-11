0c36d7df1badc20ae76e840aef5219db81fab095
JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>
2023-02-10 19:15:15 -0700

    Se agregó propiedad esTrigonometrico y Clase Tangente
    Signed-off-by: JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>

--- Dominio_LibCalc/Operacion.cs
+++ Dominio_LibCalc/Operacion.cs
@@ -11,6 +11,15 @@ namespace Dominio_LibCalc
         public string nombre = "Clase BASE";
         public double Ope1 { get; set; }
         public double Ope2 { get; set; }
+        //nueva prop para determinar si la operacion es TRIGONOMETRICA
+        protected bool esTrigonometrica = false;
+        public bool EsTrigonometrica
+        {
+            get {
+                return this.esTrigonometrica;
+            }
+        }
+
         public Operacion(double op1, double op2)
         {
             this.Ope1 = op1;
---
+++ Dominio_LibCalc/Tangente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Dominio_LibCalc
+{
+	public class Tangente : Operacion
+	{
+		public Tangente(double op1) : base( op1, new Double() )
+		{
+			//inicializamos los operandos
+			this.Ope1 = op1;
+		}
+
+
+		//override del met abstracto ejecutra
+		public override double ejecutar()
+		{
+			return Math.Tan(this.Ope1);
+		}
+	}
+}
+
